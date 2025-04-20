e3065cd3217ac314a8a42a1a4007325ff69da1e6
ialzouby <ialzouby@gmail.com>
2025-04-19 20:41:02 -0400

    script removal

--- scripts/utils/createGLContext.js
+++
@@ -1,6 +0,0 @@
-// scripts/utils/createGLContext.js
-import gl from 'gl';
-
-export function createGLContext(width = 512, height = 512) {
-  return gl(width, height, { preserveDrawingBuffer: true });
-}

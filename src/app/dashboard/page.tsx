45e23ca8468961a724a1412d9ec11ae42955fdd6
Issam Alzouby <85916427+Ialzouby@users.noreply.github.com>
2025-10-09 15:39:21 -0400

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
-export const dynamic = "force-dynamic";
 "use client"
+export const dynamic = "force-dynamic";
 import { useEffect, useRef, useState } from "react"
 import { useRouter, useSearchParams } from "next/navigation"
 import Canvas from "@/components/Canvas"
